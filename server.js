3aa60c447d26654794058429d26b683c051c0306
Smock <martini.e@gmx.de>
2023-01-13 11:39:17 +0100

    frontend post try

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.post('/frontend', (req, res) => {
+  res.send('Hello from A!');
+})
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/frontend/verifyLicensePlate.html"));
 })
+
+app.use('/',(req,res,next) => {
+  res.sendFile(path.join(__dirname + "/frontend/verifyLicensePlate.html"));
+});
 
 const server = app.listen(5000);
 const portNumber = server.address().port;
