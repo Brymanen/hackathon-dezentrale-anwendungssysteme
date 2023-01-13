be991a5de9c044e1456990df2d631b812d587088
Smock <martini.e@gmx.de>
2023-01-13 17:26:17 +0100

    added frontend for saving a license plate

--- server.js
+++ server.js
@@ -6,13 +6,24 @@ app.post('/frontend', (req, res) => {
   res.send('Hello from A!');
 })
 
+
+//for the parkingDuration.html use the following:
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/frontend/verifyLicensePlate.html"));
+    res.sendFile(path.join(__dirname + "/frontend/parkingDuration.html"));
 })
 
 app.use('/',(req,res,next) => {
-  res.sendFile(path.join(__dirname + "/frontend/verifyLicensePlate.html"));
+  res.sendFile(path.join(__dirname + "/frontend/parkingDuration.html"));
 });
+
+//for the verifyLicensePlate.html instead use the following:
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname + "/frontend/verifyLicensePlate.html"));
+// })
+//
+// app.use('/',(req,res,next) => {
+//   res.sendFile(path.join(__dirname + "/frontend/verifyLicensePlate.html"));
+// });
 
 const server = app.listen(5000);
 const portNumber = server.address().port;
