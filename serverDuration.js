203624ff35ad6d367f775405e9c37195c1729e17
Smock <martini.e@gmx.de>
2023-01-17 12:26:06 +0100

    final version uploaded

--- serverDuration.js
+++ serverDuration.js
@@ -8,13 +8,13 @@ app.post('/frontend', (req, res) => {
 
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/frontend/verifyLicensePlate.html"));
+    res.sendFile(path.join(__dirname + "/frontend/parkingDuration.html"));
 })
 
 app.use('/',(req,res,next) => {
-  res.sendFile(path.join(__dirname + "/frontend/verifyLicensePlate.html"));
+  res.sendFile(path.join(__dirname + "/frontend/parkingDuration.html"));
 });
 
-const server = app.listen(5000);
+const server = app.listen(5001);
 const portNumber = server.address().port;
 console.log(`port is open on ${portNumber}`);
